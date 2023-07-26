0375ff1b53144c8dc726ff42c79c1bf4442ea548
root <root@DESKTOP-LEB5FG3>
2023-07-26 19:32:29 +0500

    work kuber master

--- kubernetes/k8s/terraform/main.tf
+++ kubernetes/k8s/terraform/main.tf
@@ -13,8 +13,8 @@ terraform {
   }
 }
 
-resource "yandex_compute_instance" "kube_master" {
-  name        = "kube_master"
+resource "yandex_compute_instance" "kuber" {
+  name        = "kube-master"
   platform_id = "standard-v2"
   count = var.count_ci
 
@@ -30,6 +30,7 @@ resource "yandex_compute_instance" "kube_master" {
     initialize_params {
       size = 40
       image_id = var.app_disk_image
+      type = "network-ssd"
     }
   }
 
@@ -39,71 +40,66 @@ resource "yandex_compute_instance" "kube_master" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+    ssh-keys = "debian:${file(var.public_key_path)}"
   }
 
   connection {
     type  = "ssh"
     host  = self.network_interface.0.nat_ip_address
-    user  = "ubuntu"
+    user  = "debian"
     agent = false
     # путь до приватного ключа
     private_key = file(var.private_key_path)
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.network_interface.0.nat_ip_address},' --extra-vars 'private_ip=${self.private_ip} hostname=${self.name} public_ip=${self.network_interface.0.nat_ip_address}' --private-key ${var.private_key_path} ../ansible/master-playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.network_interface.0.nat_ip_address},' --extra-vars 'private_ip=${self.network_interface.0.ip_address} hostname=${self.name} public_ip=${self.network_interface.0.nat_ip_address}' --private-key ${var.private_key_path} ../ansible/master-playbook.yml"
   }
 }
 
 
-#
-#resource "yandex_compute_instance" "kube_node" {
-#  name        = "kube-node-${count.index}"
-#  platform_id = "standard-v2"
-#  count = var.count_ci
-#
-#  labels = {
-#    tags = "docker-app"
-#  }
-#  resources {
-#    cores         = 2
-#    memory        = 4
-#    core_fraction = 20
-#  }
-#
-#  boot_disk {
-#    initialize_params {
-#      size = 40
-#      image_id = var.app_disk_image
-#    }
-#  }
-#
-#  network_interface {
-#    subnet_id = var.subnet_id
-#    nat       = true
-#  }
-#
-#  metadata = {
-#    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-#  }
-#
-#  connection {
-#    type  = "ssh"
-#    host  = self.network_interface.0.nat_ip_address
-#    user  = "ubuntu"
-#    agent = false
-#    # путь до приватного ключа
-#    private_key = file(var.private_key_path)
-#  }
-#}
-#
-#resource "local_file" "AnsibleInventory" {
-# content = templatefile("ansible_inventory.tmpl",
-#   {
-#    hostnames         = yandex_compute_instance.app.*.name,
-#    ansible_hosts     = yandex_compute_instance.app[*].network_interface.0.nat_ip_address,
-#   }
-# )
-# filename = "dynamic_inventory"
-#}
+
+resource "yandex_compute_instance" "worker" {
+  name        = "kube-worker-${count.index}"
+  platform_id = "standard-v2"
+  count = var.count_ci
+
+  labels = {
+    tags = "docker-app"
+  }
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 50
+  }
+
+  boot_disk {
+    initialize_params {
+      size = 40
+      image_id = var.app_disk_image
+      type = "network-ssd"
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "debian:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface.0.nat_ip_address
+    user  = "debian"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.network_interface.0.nat_ip_address},' --extra-vars 'private_ip=${self.network_interface.0.ip_address} hostname=${self.name} public_ip=${self.network_interface.0.nat_ip_address}' --private-key ${var.private_key_path} ../ansible/worker-playbook.yml"
+  }
+}
